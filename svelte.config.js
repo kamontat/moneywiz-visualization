b3cf7c60101a0670f0d88839700d77f360f4fa6c
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-24 14:59:06 +0700

    Add prerendering for static deployment and clean up Svelte config
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
