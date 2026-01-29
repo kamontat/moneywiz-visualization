7a07b2d19b0299f4a773d61aa1531864c9a6ea14
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-29 19:34:23 +0700

    refactor: restructure loggers to use lib for extending log categories
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- src/lib/loggers/constants.ts
+++ src/lib/loggers/constants.ts
@@ -1,9 +1,11 @@
 import { Log } from './models'
 
-export const csv = Log.root.extends('csv')
-export const transaction = Log.root.extends('transaction')
-export const localStorage = Log.root.extends('localStorage')
-export const store = Log.root.extends('store')
+export const lib = Log.root.extends('lib')
+export const csv = lib.extends('csv')
+export const transaction = lib.extends('transaction')
+export const localStorage = lib.extends('localStorage')
+export const store = lib.extends('store')
+export const analytics = lib.extends('analytics')
 
 export const component = Log.root.extends('component')
 export const page = Log.root.extends('page')
