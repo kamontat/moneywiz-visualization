a8c744870915c68dff1da32af4f4efc646f73946
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-31 15:10:20 +0700

    feat: add AppHeader and AppFooter components for improved layout and functionality
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

---
+++ src/components/organisms/AppBody.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { BaseProps } from '$lib/components/models'
+	import Body from '$components/atoms/Body.svelte'
+
+	type Props = BaseProps
+
+	let { children, class: className }: Props = $props()
+</script>
+
+<Body class={className}>
+	{@render children?.()}
+</Body>
---
+++ src/components/organisms/AppHeader.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Container from '$components/atoms/Container.svelte'
+	import Header from '$components/atoms/Header.svelte'
+	import CsvClearButton from '$components/molecules/CsvClearButton.svelte'
+	import CsvUploadButton from '$components/molecules/CsvUploadButton.svelte'
+	import NameHeader from '$components/molecules/NameHeader.svelte'
+	import ThemeSelect from '$components/molecules/ThemeSelect.svelte'
+</script>
+
+<Header>
+	<NameHeader />
+	<Container class="items-stretch gap-x-3">
+		<Container class="d-join">
+			<CsvClearButton class="d-join-item" />
+			<CsvUploadButton class="d-join-item" />
+		</Container>
+
+		<ThemeSelect />
+	</Container>
+</Header>
