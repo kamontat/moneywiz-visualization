604bad1be1c5208d63dbe919879fd96e7afdabbc
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-02-05 21:48:20 +0700

    chore: remove unused type exports from transaction models
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- src/lib/transactions/index.ts
+++ src/lib/transactions/index.ts
@@ -3,14 +3,3 @@
 // export const trxStore = initTrxStore()
 export { parseTransactions, parseTransactionsFile } from './parser'
 export { getValue, CsvKey } from './csv'
-
-export type {
-	ParsedAccount,
-	ParsedAccountType,
-	ParsedAmount,
-	ParsedCategory,
-	ParsedTag,
-	ParsedTransactions,
-	ParsedTransaction,
-	ParsedTransactionType,
-} from './models'
